8acd6dc31ab91cb4fdd2fdb44724dabccac2bfc7
JeongSeok <ljs0429777@gmail.com>
2024-07-23 01:55:12 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/est/cranejob/CraneJobApplication.java
@@ -0,0 +1,13 @@
+package com.est.cranejob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraneJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraneJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/est/cranejob/CraneJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.est.cranejob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraneJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
