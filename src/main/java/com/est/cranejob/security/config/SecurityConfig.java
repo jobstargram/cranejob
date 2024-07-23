bf3e5ff26d1ce710040a632a65533757bb19c3bc
JeongSeok <ljs0429777@gmail.com>
2024-07-24 01:24:05 +0900

    feat: 로그인, 회원가입 컨트롤러와 서비스 구현

--- src/main/java/com/est/cranejob/security/config/SecurityConfig.java
+++ src/main/java/com/est/cranejob/security/config/SecurityConfig.java
@@ -1,8 +1,7 @@
-package com.est.cranejob.config;
+package com.est.cranejob.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -21,7 +20,7 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/css/**", "/images/**", "/js/**", "/favicon.*", "/*/icon-*").permitAll()
-				.requestMatchers("/").permitAll()
+				.requestMatchers("/", "/user/signup").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin(form -> form
--- src/main/java/com/est/cranejob/user/service/UserService.java
+++ src/main/java/com/est/cranejob/user/service/UserService.java
@@ -1,8 +1,13 @@
 package com.est.cranejob.user.service;
 
+import com.est.cranejob.user.dto.CreateUser;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+	public void createUser(CreateUser createUser) {
+		System.out.println(createUser.getName());
+	}
 
 }
