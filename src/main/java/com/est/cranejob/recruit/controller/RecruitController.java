566e8ee772559524077f1fc519e49f4cdbed0709
zoanna <zoanna5442@gmail.com>
2024-07-23 21:55:11 +0900

    html/css add and thymeleaf settings

--- src/main/java/com/est/cranejob/recruit/controller/RecruitController.java
+++ src/main/java/com/est/cranejob/recruit/controller/RecruitController.java
@@ -1,8 +1,17 @@
 package com.est.cranejob.recruit.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/recruit")
 public class RecruitController {
+
+    @GetMapping
+    public String recruit(Model model) {
+        return "recruit/recruit-list";
+    }
 
 }
