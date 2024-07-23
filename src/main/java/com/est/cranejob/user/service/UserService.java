0a6af09facc0e79e04cfa3b2ed4a0bdc2c411079
JeongSeok <ljs0429777@gmail.com>
2024-07-24 02:33:19 +0900

    feat: 회원가입 기능 추가 맟 엔티티 리펙토링

--- src/main/java/com/est/cranejob/user/service/UserService.java
+++ src/main/java/com/est/cranejob/user/service/UserService.java
@@ -1,13 +1,18 @@
 package com.est.cranejob.user.service;
 
 import com.est.cranejob.user.dto.CreateUser;
+import com.est.cranejob.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+	private final UserRepository userRepository;
+
 	public void createUser(CreateUser createUser) {
-		System.out.println(createUser.getName());
+		userRepository.save(createUser.toEntity());
 	}
 
 }
