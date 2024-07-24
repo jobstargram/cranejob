59b00c20f51a52205bc34eb862b7d2dcf1d703c5
JeongSeok <ljs0429777@gmail.com>
2024-07-25 02:47:07 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/est/cranejob/user/controller/UserController.java
+++ src/main/java/com/est/cranejob/user/controller/UserController.java
@@ -2,8 +2,13 @@ package com.est.cranejob.user.controller;
 
 import com.est.cranejob.user.dto.request.CreateUserRequest;
 import com.est.cranejob.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,4 +49,16 @@ public class UserController {
 	public String userEditForm() {
 		return "/user/edit";
 	}
+
+	@GetMapping("/user/logout")
+	public String userLogout(HttpServletRequest request, HttpServletResponse response) {
+		Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
+
+		if (authentication != null) {
+			new SecurityContextLogoutHandler().logout(request, response, authentication);
+		}
+
+		return "redirect:/";
+	}
+
 }
