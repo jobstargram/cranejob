4237b8b58dee4dc99c8e35fabb899138a3214817
jojunho <junhosvain@naver.com>
2024-07-24 10:25:44 +0900

    feat: 게시글, 댓글 엔티티 작성

--- src/main/java/com/est/cranejob/post/domain/Post.java
+++ src/main/java/com/est/cranejob/post/domain/Post.java
@@ -1,5 +1,54 @@
 package com.est.cranejob.post.domain;
 
+import com.est.cranejob.comment.domain.Comment;
+import com.est.cranejob.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long postId;
+
+    // 게시글 작성자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // 게시글의 댓글 리스트
+    @OneToMany(mappedBy = "post", fetch = FetchType.LAZY)
+    private List<Comment> postCommentList;
+
+    @Column(name = "post_title")
+    private String title;
+
+    @Lob
+    @Column(name = "post_content")
+    private String postContent;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
