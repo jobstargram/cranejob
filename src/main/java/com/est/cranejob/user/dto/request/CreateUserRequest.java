821acb1cb0226196805120697e7d13d85bb7efcf
JeongSeok <ljs0429777@gmail.com>
2024-07-25 13:11:17 +0900

    style: depth 수정

--- src/main/java/com/est/cranejob/user/dto/request/CreateUserRequest.java
+++ src/main/java/com/est/cranejob/user/dto/request/CreateUserRequest.java
@@ -18,21 +18,19 @@ import lombok.Setter;
 @Builder
 public class CreateUserRequest {
 
-  // RFC 5322 이메일 정규식
-  @Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"
-          ,message = "이메일을 입력해 주세요.")
-  @NotBlank(message = "email은 빈칸을 입력할 수 없습니다.")
+	// RFC 5322 이메일 정규식
+	@Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "이메일을 입력해 주세요.")
+	@NotBlank(message = "email은 빈칸을 입력할 수 없습니다.")
 	private String username;
-  
-  // 비밀번호: 영문, 숫자, 특수문자를 포함하여 8~15자 작성
-  @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$"
-          ,message = "비밀번호는 10글자 ~ 15글자 사이여야 합니다")
-  @NotBlank(message = "비밀번호는 빈칸을 입력할 수 없습니다.")
+
+	// 비밀번호: 영문, 숫자, 특수문자를 포함하여 8~15자 작성
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 10글자 ~ 15글자 사이여야 합니다")
+	@NotBlank(message = "비밀번호는 빈칸을 입력할 수 없습니다.")
 	private String password;
-  
-  @NotBlank(message = "이름은 빈칸을 입력할 수 없습니다.")
+
+	@NotBlank(message = "이름은 빈칸을 입력할 수 없습니다.")
 	private String nickname;
-  
+
 	private UserStatus userStatus;
 	private Role role;
 
