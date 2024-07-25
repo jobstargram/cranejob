5e817ef39614daae7fed44725cdbc5bbe426fc46
JeongSeok <ljs0429777@gmail.com>
2024-07-26 01:01:16 +0900

    feat: validation 추가

--- src/main/java/com/est/cranejob/user/dto/request/CreateAdminUserRequest.java
+++ src/main/java/com/est/cranejob/user/dto/request/CreateAdminUserRequest.java
@@ -19,12 +19,12 @@ import lombok.Setter;
 public class CreateAdminUserRequest {
 
 	// RFC 5322 이메일 정규식
-	@Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "이메일을 입력해 주세요.")
-	@NotBlank(message = "email은 빈칸을 입력할 수 없습니다.")
+	@Pattern(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "아이디는 이메일 형태로 입력해주세요.")
+	@NotBlank(message = "아이디는 빈칸을 입력할 수 없습니다.")
 	private String username;
 
 	// 비밀번호: 영문, 숫자, 특수문자를 포함하여 8~15자 작성
-	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$", message = "비밀번호는 10글자 ~ 15글자 사이여야 합니다")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,}$", message = "비밀번호는 영문, 숫자, 특수문자를 포함하여 최소 8글자 이상이어야 합니다.")
 	@NotBlank(message = "비밀번호는 빈칸을 입력할 수 없습니다.")
 	private String password;
 
