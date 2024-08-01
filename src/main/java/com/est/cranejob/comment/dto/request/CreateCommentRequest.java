3f68f813df78999a7fe838713c55307aff0d40c8
jojunho <junhosvain@naver.com>
2024-08-02 03:46:05 +0900

    feat: CommentDTO(Response) 어노테이션 수정, 댓글 작성시 isDelete가 null로 나오는 문제 해결, 폴더명 변경(PostController, postDetail)

--- src/main/java/com/est/cranejob/comment/dto/request/CreateCommentRequest.java
+++ src/main/java/com/est/cranejob/comment/dto/request/CreateCommentRequest.java
@@ -1,6 +1,7 @@
 package com.est.cranejob.comment.dto.request;
 
 import com.est.cranejob.comment.domain.Comment;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -19,13 +20,15 @@ import java.io.Serializable;
 @Builder
 public class CreateCommentRequest implements Serializable {
 
-    @NotNull(message = "댓글의 내용은 비어있을 수 없습니다.")
+    @NotBlank(message = "댓글의 내용은 비어있을 수 없습니다.")
     @Size(max = 100, message = "댓글은 100자 이내로 작성 되어야 합니다.")
     private String content;
+    private Boolean isDeleted = false;
 
     public Comment toEntity(){
         return Comment.builder()
                 .content(content)
+                .isDeleted(isDeleted)
                 .build();
     }
 }
